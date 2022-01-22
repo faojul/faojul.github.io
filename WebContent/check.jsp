daad3933724d7adbfd325fb6cf7087f207dd8f7c
faojul <faojulahsan@gmail.com>
2022-01-22 13:19:02 +0600

    initial Commit for portfolio

--- WebContent/check.jsp
+++
@@ -1,4 +0,0 @@
-<form action="Update" method="post">
-<input type="text" name="id" />
-<input type="submit" value="Submit"/>
-</form>
--- WebContent/include/footer.php
+++
@@ -1,7 +0,0 @@
-	<div class="footer">
-		<div class="container">
-			 
-
-			<b class="copyright">&copy; 2017 Online Agriculture System </b> All rights reserved.
-		</div>
-	</div>
